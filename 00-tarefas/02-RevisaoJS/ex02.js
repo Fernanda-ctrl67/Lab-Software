fde1c3646d6f544d4ed1e406ab9c2f7e83b35b5c
Fernanda-ctrl67 <donaldpato892@gmail.com>
2026-07-03 14:29:14 -0300

    exrcicios feitos 03.07

--- 00-tarefas/02-RevisaoJS/ex02.js
+++ 00-tarefas/02-RevisaoJS/ex02.js
@@ -8,3 +8,17 @@
 // cidade
 
 // Depois imprima todas elas e em seguida imprima seus tipos.
+let nome = "Nanda"
+let sobrenome = "Ferreira"
+let idade = 17
+let cidade = "aVasconcellos"
+
+console.log(nome);
+console.log(sobrenome);
+console.log(idade);
+console.log(cidade);
+
+console.log(typeof nome);
+console.log(typeof sobrenome);
+console.log(typeof idade);
+console.log(typeof cidade);
